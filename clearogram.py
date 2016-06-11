74052b30d7e439d241c0a32a1cece2a19f350372
Meredith Rawls <meredith.rawls@gmail.com>
2016-06-11 15:58:04 +0200

    plots either points or a single ellipse

--- clearogram.py
+++ clearogram.py
@@ -2,6 +2,9 @@ from __future__ import division, print_function
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy.io import ascii
+from matplotlib.patches import Ellipse
+import matplotlib.patches as patches
+import numpy.random as rnd
 #from astropy.table import Table
 """
 De-confusifying the Confusogram
@@ -17,7 +20,21 @@ by Meredith Rawls and friends
 infile = 'summary-toupies-v3-send.dat'
 
 data = ascii.read(infile, format='commented_header')
+#print(data)
 
-print(data)
+#ells = [Ellipse(xy=rnd.rand(2)*10, width=rnd.rand(), height=rnd.rand(), angle=rnd.rand()*360)
+#        for i in range(50)]
 
-#mass, prot, ross, stov, mag1, mag2 = 
+fig1 = plt.figure()
+plt.plot(data['P_rot'], data['M'], ls='None', marker='o', markersize=10)
+ax1 = fig1.add_subplot(111)
+plt.xlabel(r'$P_{\textrm{rot}}$ (d)')
+plt.ylabel(r'Mass ($M_{\odot}$)')
+
+myellipse = Ellipse(xy=(0.5, 0.5), width=0.1, height=0.2)
+
+#ax1.add_patch(myellipse)
+
+plt.show()
+
+
