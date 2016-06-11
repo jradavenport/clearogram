b1c41cd34525b063e351d79b16d020a1ce23c030
Meredith Rawls <meredith.rawls@gmail.com>
2016-06-11 11:33:15 +0200

    begin python plotting routine

---
+++ clearogram.py
@@ -0,0 +1,13 @@
+from __future__ import division, print_function
+import numpy as np
+import matplotlib.pyplot as plt
+"""
+De-confusifying the Confusogram
+Cool Stars 19 Hack Day
+by Meredith Rawls and friends
+"""
+
+# Read in confusogram data
+# Plot it
+# Plot it better
+
